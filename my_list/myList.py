90391fbbee0b550f6673336193c69ae676d008f0
mo-mosverkstad <34004324+mo-mosverkstad@users.noreply.github.com>
2017-12-09 11:47:17 +0100

    MyList: add more functions for len, index and []
    Add more functions for member variable length, methods index and override the operator [].

--- my_list/myList.py
+++ my_list/myList.py
@@ -12,12 +12,14 @@ class MyList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def append(self, value):
         n = Node(value)
         if self.tail != None: self.tail.next = n
         self.tail = n
         if self.head == None: self.head = n
+        self.length += 1
 
     def get(self, index):
         v = self.head
@@ -38,8 +40,27 @@ class MyList:
             i += 1
         n.next = h.next
         h.next = n
-        
-        
+        self.length += 1
+
+    def index(self, value):
+        h = self.head
+        i = 0
+        while h != None and h.value != value:
+            h = h.next
+            i += 1
+        if h == None: return -1
+        elif h.value == value: return i
+
+    def __getitem__(self, index):
+        return self.get(index)
+
+    def __setitem__(self, index, value):
+        v = self.head
+        i = 0
+        while i < index and v != None:
+            v = v.next
+            i += 1
+        if i == index: v.value = value
 
     def __str__(self):
         n = self.head
@@ -50,4 +71,3 @@ class MyList:
             n = n.next
             i += 1
         return strValue
-        
