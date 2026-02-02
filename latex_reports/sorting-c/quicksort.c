674a77f1edd2b3e5dfab22313dcaedeaf67551d4
mo-mosverkstad <mo.mosverkstad@gmail.com>
2026-02-02 21:28:11 +0100

    Correct quicksort algorithm

--- latex_reports/sorting-c/quicksort.c
+++ latex_reports/sorting-c/quicksort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void quick_sort_inner(int *arr, unsigned start, unsigned int end){
+void quick_sort_inner(int *arr, unsigned int start, unsigned int end){
     if (start >= end) return;
     if (end - start <= 1) return;
     unsigned int pivot_index = (start + end)/2;
@@ -23,6 +23,31 @@ void quick_sort_inner(int *arr, unsigned start, unsigned int end){
     quick_sort_inner(arr, start, i);
     quick_sort_inner(arr, i, end);
 }
+
+void quick_sort_inner(int *arr, unsigned int start, unsigned int end) {
+    if (end - start <= 1) return;
+    unsigned int pivot_index = start + (end - start) / 2;
+    int pivot_value = arr[pivot_index];
+    unsigned int i = start;
+    unsigned int j = end - 1;
+
+    while (i <= j) {
+        while (i < end && arr[i] < pivot_value) i++;
+        while (j > start && arr[j] > pivot_value) j--;
+
+        if (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            if (j > 0) j--;
+        }
+    }
+
+    quick_sort_inner(arr, start, j + 1);
+    quick_sort_inner(arr, i, end);
+}
+
 
 void quicksort(int *arr, unsigned int n){
     if (arr == NULL || n == 0) return;
